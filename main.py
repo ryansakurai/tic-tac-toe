63ea682a5a0918b049b7d2ca3c11805965146ca5
Ryan Sakurai <ryansakurai03@gmail.com>
2022-11-15 20:02:32 -0300

    documentation added

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import random
+from typing import Union
 
 
+## character used to represent an empty space in the board
 EMPTY_SPACE = '-'
+## string used as divisor in the UI
 DIV = '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
 
 
-def print_board(board):
+def print_board(board: list) -> None:
+    """
+    Prints the 1D board as a 2D 3x3 board
+
+    Parameters:
+        board: list with 10 spots representing the board (the first one is not used)
+    Returns:
+        Nothing
+    """
+
     # row by row
     for i in range(1, 8, 3):
         print( f'{board[i]}\t{board[i+1]}\t{board[i+2]}' )
         print()
 
 
-def read_number(command):
+def read_number(command: str) -> int:
+    """
+    Reads a number between 1-9 from the user and returns it
+
+    Parameters:
+        command: string to be passed as argumeto to input()
+    Returns:
+        The number read
+    """
+
     while True:
         num = input(command)
         if not num.isdigit():
@@ -25,7 +46,16 @@ def read_number(command):
                 return num
 
 
-def read_coordinates(board):
+def read_position(board: list) -> int:
+    """
+    Reads the empty position where the player wants to put his mark
+
+    Parameters:
+        board: list with 10 spots representing the board (the first one is not used)
+    Returns:
+        The position read
+    """
+
     while True:
         position = read_number('Where will you place your mark (1-9)? ')
         if board[position] == EMPTY_SPACE:
@@ -35,7 +65,17 @@ def read_coordinates(board):
     return position
 
 
-def verify_win(board, mark):
+def verify_win(board: list, mark: str) -> bool:
+    """
+    Checks if the owner of the mark has won
+
+    Parameters:
+        board: list with 10 spots representing the board (the first one is not used)
+        mark: mark used by the player
+    Returns:
+        If the player has won
+    """
+
     # primary diagonal
     if board[1] == board[5] == board[9] == mark:
         return True
@@ -54,11 +94,30 @@ def verify_win(board, mark):
         return False
 
 
-def verify_tie(board):
+def verify_tie(board: list) -> bool:
+    """
+    Checks if there was a tie
+
+    Parameters:
+        board: list with 10 spots representing the board (the first one is not used)
+    Returns:
+        If there was a tie
+    """
+
     return EMPTY_SPACE not in board
 
 
-def play_game(players):
+def play_game(players: Union[list, tuple]) -> None:
+    """
+    Starts the game and ends it in case of a win or tie
+
+    Parameters:
+        players: iterable collection containing the players, which are
+                 a dictionary containing their name and mark
+    Returns:
+        Nothing
+    """
+
     random.shuffle(players)
     board = [None] + [EMPTY_SPACE]*9
     done = False
@@ -66,7 +125,7 @@ def play_game(players):
         for player in players:
             print_board(board)
             print(f"<< {player['name']} >>")
-            position = read_coordinates(board)
+            position = read_position(board)
             board[position] = player['mark']
 
             print()
